e794c5cbbc2857a997c93415e6bf1063a33fe7f1
r0kias <96231967+r0kias@users.noreply.github.com>
2022-03-28 13:42:38 +0300

    Update 5-main.c

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -1,18 +1,27 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * *_strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to look for
  *
- * Return: Always 0.
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found
  */
-int main(void)
+char *_strstr(char *haystack, char *needle)
 {
-	char *s = "wworld of this";
-	char *f = "world";
-	char *t;
+	int i, j;
 
-	t = _strstr(s, f);
-	printf("%s\n", t);
-	return (0);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (NULL);
 }
